3caf4e9472b3d393e34be7866484c9cc522137c9
Steven Ramdas <steven.ramdas@vattenfall.com>
2023-08-08 16:25:32 +0200

    Implement model tracking with MLflow and implement data splitting

--- src/model/train.py
+++ src/model/train.py
@@ -1,26 +1,18 @@
-# Import libraries
-
 import argparse
 import glob
 import os
-
+import mlflow
 import pandas as pd
-
 from sklearn.linear_model import LogisticRegression
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 
-# define functions
 def main(args):
-    # TO DO: enable autologging
-
-
-    # read data
+    mlflow.autolog() 
+    mlflow.start_run()
     df = get_csvs_df(args.training_data)
-
-    # split data
     X_train, X_test, y_train, y_test = split_data(df)
-
-    # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
@@ -33,42 +25,38 @@ def get_csvs_df(path):
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
 
 
-# TO DO: add function to split data
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    mlflow.log_param("X", X)
 
+    mlflow.log_param("unique_count", np.unique(y, return_counts=True))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
-    # train model
-    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
-
+    return LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    
 
 def parse_args():
-    # setup arg parser
     parser = argparse.ArgumentParser()
 
-    # add arguments
-    parser.add_argument("--training_data", dest='training_data',
-                        type=str)
-    parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+    parser.add_argument(
+        "--training_data", 
+        dest='training_data',
+        type=str
+    )
+    parser.add_argument(
+        "--reg_rate", 
+        dest='reg_rate',
+        type=float, 
+        default=0.01
+    )
 
-    # parse args
     args = parser.parse_args()
 
-    # return args
     return args
 
-# run script
 if __name__ == "__main__":
-    # add space in logs
-    print("\n\n")
-    print("*" * 60)
-
-    # parse args
     args = parse_args()
-
-    # run main function
     main(args)
-
-    # add space in logs
-    print("*" * 60)
-    print("\n\n")
